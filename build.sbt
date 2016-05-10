8a0da734589c9bdac31b4968c495404241f8510a
Simon Hildrew <simon.hildrew@theguardian.com>
2016-05-10 15:03:38 +0100

    Clean up deps and ordering of config

--- build.sbt
+++ build.sbt
@@ -6,13 +6,16 @@ scalaVersion in ThisBuild := "2.11.8"
 description   := "AWS lambdas to snapshot Flexible content to S3"
 scalacOptions ++= Seq("-deprecation", "-feature", "-unchecked")
 
+val awsVersion = "1.10.75"
+val playVersion = "2.5.0"
+
 libraryDependencies ++= Seq(
   "com.amazonaws" % "aws-lambda-java-core" % "1.1.0",
   "com.amazonaws" % "aws-lambda-java-events" % "1.1.0",
   "com.amazonaws" % "aws-lambda-java-log4j" % "1.0.0",
   "org.slf4j" % "slf4j-log4j12" % "1.7.21",
-  "com.amazonaws" % "aws-java-sdk-s3" % "1.10.39",
-  "com.typesafe.play" %% "play-json" % "2.5.0",
-  "com.typesafe.play" %% "play-ws" % "2.5.0",
+  "com.amazonaws" % "aws-java-sdk-s3" % awsVersion,
+  "com.typesafe.play" %% "play-json" % playVersion,
+  "com.typesafe.play" %% "play-ws" % playVersion,
   "org.scalatest" %% "scalatest" % "2.2.5" % "test"
 )
--- src/main/scala/com/gu/flexible/snapshotter/Config.scala
+++ src/main/scala/com/gu/flexible/snapshotter/Config.scala
@@ -1,39 +1,46 @@
 package com.gu.flexible.snapshotter
 
 import com.amazonaws.regions.{Region, Regions}
+import com.amazonaws.services.lambda.runtime.Context
 
 object Config {
   def apply() = new ProdConfig()
+
+  def guessStage(context: Context): Option[String] =
+    context.getFunctionName.split(Array('-','_')).toList.filter(_.length > 0).lastOption
 }
 
 sealed trait Config {
-  def bucket: String
   def apiUrl: String
-  def kinesisStream: String
-  def region: Region = Regions.getCurrentRegion
+  def bucket: String
   def kmsKey: Option[String]
+  def kinesisStream: String
+
+  def region: Region = Regions.getCurrentRegion
 
   def contentUri = s"$apiUrl/content"
   def contentRawUri = s"$apiUrl/contentRaw"
 }
 
 class DevConfig extends Config {
-  override val bucket: String = "flexible-snapshots-dev"
-  override val kinesisStream: String = ???
   override val apiUrl: String = "http://internal-Flexible-ApiLoadB-15RTA1C81ZYGU-432053948.eu-west-1.elb.amazonaws.com:8080"
+  override val bucket: String = "flexible-snapshots-dev"
   override val kmsKey: Option[String] = ???
+  override val kinesisStream: String = ???
 
   override val region = Region.getRegion(Regions.EU_WEST_1)
 }
 
-//class CodeConfig extends Config {
-//  override val bucket: String = "flexible-snapshots-code"
-//  override val apiUrl: String = "http://internal-Flexible-ApiLoadB-15RTA1C81ZYGU-432053948.eu-west-1.elb.amazonaws.com:8080"
-//}
+class CodeConfig extends Config {
+  override val bucket: String = "flexible-snapshots-code"
+  override val apiUrl: String = "http://internal-Flexible-ApiLoadB-15RTA1C81ZYGU-432053948.eu-west-1.elb.amazonaws.com:8080"
+  override val kmsKey: Option[String] = ???
+  override val kinesisStream: String = ???
+}
 
 class ProdConfig extends Config {
-  override val bucket: String = "flexible-snapshots-prod"
-  override val kinesisStream: String = ???
   override val apiUrl: String = "http://internal-Flexible-ApiLoadB-1QAHGRQLH03UW-649659201.eu-west-1.elb.amazonaws.com:8080"
+  override val bucket: String = "flexible-snapshots-prod"
   override val kmsKey: Option[String] = ???
+  override val kinesisStream: String = ???
 }
