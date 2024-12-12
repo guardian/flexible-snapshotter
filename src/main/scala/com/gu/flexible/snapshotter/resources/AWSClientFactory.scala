dc9ccb05ac8da7a28d6700ab530f3b08b32d1055
David Furey <david.furey@guardian.co.uk>
2024-12-12 17:29:34 +0000

    Bump dependencies to resolve high vulnerabilities
    Also minor change to squash deprecation warning

--- src/main/scala/com/gu/flexible/snapshotter/resources/AWSClientFactory.scala
+++ src/main/scala/com/gu/flexible/snapshotter/resources/AWSClientFactory.scala
@@ -1,20 +1,20 @@
 package com.gu.flexible.snapshotter.resources
 
-import com.amazonaws.regions.Regions
+import com.amazonaws.regions.{Region, RegionUtils}
 import com.amazonaws.services.cloudwatch.{AmazonCloudWatch, AmazonCloudWatchClientBuilder}
 import com.amazonaws.services.lambda.{AWSLambda, AWSLambdaClientBuilder}
 import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
 import com.amazonaws.services.sns.{AmazonSNS, AmazonSNSClientBuilder}
 
 object AWSClientFactory {
-  def getRegion = Option(System.getenv("AWS_DEFAULT_REGION")).map(Regions.fromName).get
+  def getRegion: Region = RegionUtils.getRegion(System.getenv("AWS_DEFAULT_REGION"))
 
-  def createSNSClient(implicit region:Regions): AmazonSNS =
+  def createSNSClient(implicit region:Region): AmazonSNS =
     AmazonSNSClientBuilder.standard().withRegion(region.getName).build()
-  def createS3Client(implicit region:Regions): AmazonS3 =
+  def createS3Client(implicit region:Region): AmazonS3 =
     AmazonS3ClientBuilder.standard().withRegion(region.getName).build()
-  def createLambdaClient(implicit region:Regions): AWSLambda =
+  def createLambdaClient(implicit region:Region): AWSLambda =
     AWSLambdaClientBuilder.standard().withRegion(region.getName).build()
-  def createCloudwatchClient(implicit region:Regions): AmazonCloudWatch =
+  def createCloudwatchClient(implicit region:Region): AmazonCloudWatch =
     AmazonCloudWatchClientBuilder.standard().withRegion(region.getName).build()
 }
